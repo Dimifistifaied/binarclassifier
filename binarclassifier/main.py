85ce2b7ec03b79eb395b2a2ddc3d880d611e54b9
Popov <dimitar.popov@atos.net>
2020-12-04 13:38:45 +0200

    better sentence

--- binarclassifier/main.py
+++ binarclassifier/main.py
@@ -5,7 +5,7 @@ from propositional import Propositional as pr
 Network.create_network()
 
 symbolic = sy()
-formal = symbolic.evaluator("The sun is a star and a sphere then not a planet")
+formal = symbolic.evaluator("If the sun is a star and a fiery boul of molthing lava then it is not a planet")
 propose_evl = pr()
 propose_evl.propositional_evaluation(formal)
 
